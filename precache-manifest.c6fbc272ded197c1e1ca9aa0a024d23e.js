4ac5b0bd61cb214c2a11f6557dd3d54fda539ce1
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-05-06 10:12:59 +0200

    init commit

---
+++ precache-manifest.c6fbc272ded197c1e1ca9aa0a024d23e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1789f8f357e67c354ee06e55f665d955",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f85405a17b3289b804d",
+    "url": "./static/css/main.4bcfe592.chunk.css"
+  },
+  {
+    "revision": "cf96070990999b9e4a85",
+    "url": "./static/js/2.6bdcfe48.chunk.js"
+  },
+  {
+    "revision": "3f85405a17b3289b804d",
+    "url": "./static/js/main.fdd0843a.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
